5b612097965dc0c2fb000affe1decd820249664a
Fai <lamkafai1997@gmail.com>
2025-04-16 23:07:08 -0400

    feat: update Brewfile and zsh completions

--- home/.chezmoiscripts/run_after_100-brew-install.sh
+++ home/.chezmoiscripts/run_after_100-brew-install.sh
@@ -2,10 +2,10 @@
 
 if ! command -v brew &>/dev/null; then
   echo "Installing hmebrew"
-  NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if command -v brew &>/dev/null; then
   echo "Installing libaries and applications with homebrew"
-  NONINTERACTIVE=1 $HOMEBREW_PREFIX/bin/brew bundle install --global || true
+  $HOMEBREW_PREFIX/bin/brew bundle install --global || true
 fi
