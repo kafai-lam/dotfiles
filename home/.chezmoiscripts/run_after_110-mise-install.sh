f5d99b67f11a273eb7a8eaa7280caf65dcc0c10e
Fai <lamkafai1997@gmail.com>
2025-08-15 00:24:01 -0400

    chore(chezmoi): Upgrade mise on chezmoi update

--- home/.chezmoiscripts/run_after_110-mise-install.sh
+++ home/.chezmoiscripts/run_after_110-mise-install.sh
@@ -3,6 +3,7 @@
 if command -v mise &>/dev/null; then
   echo "Installing packages with mise"
   mise install --cd $HOME || true
+  mise upgrade
 else
   echo "Missing mise. Skipping mise install"
 fi
