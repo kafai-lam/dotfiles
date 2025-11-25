e8c4a5ff7009babd7ad08dea1d945ed390b84503
Fai <lamkafai1997@gmail.com>
2025-11-25 11:30:53 -0500

    refeactor(zsh): Modify shell completion generation

--- home/dot_config/zsh/plugins/function.plugin.zsh
+++
@@ -1,4 +0,0 @@
-(( $+commands[claude-squad] )) && eval "$(claude-squad completion zsh)"
-(( $+commands[codex] )) && eval "$(codex completion zsh)"
-(( $+commands[llm] )) && eval "$(_LLM_COMPLETE=zsh_source llm)"
-(( $+commands[uv] )) && eval "$(uv generate-shell-completion zsh)"
