eb6e0c3433719587a4e46398a18c3c231f73f03d
Fai <lamkafai1997@gmail.com>
2025-12-19 16:24:16 -0500

    feat(aichat): Add aichat integration
    Install aichat via Homebrew and add zsh plugin for shell integration
    Configure default model and API settings for local Bifrost instance

---
+++ home/dot_config/zsh/plugins/aichat.plugin.zsh
@@ -0,0 +1,21 @@
+if (( ! $+commands[aichat] )); then
+  return
+fi
+
+_aichat_zsh() {
+  [[ -z "$BUFFER" ]] && return
+
+  local _old=$BUFFER
+
+  zle -I
+  zle redisplay
+
+  local _out
+  _out=$(gum spin --title "convert..." -- aichat -e "$_old")
+
+  BUFFER=$_out
+  zle end-of-line
+}
+zle -N _aichat_zsh
+
+z4h bindkey _aichat_zsh "Option+\\"
