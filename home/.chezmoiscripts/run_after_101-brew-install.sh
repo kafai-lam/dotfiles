29f5db1523d1a9a91438f68b7d4dc012c4f550f6
Fai <lamkafai1997@gmail.com>
2026-01-13 11:58:15 -0500

    chore(brew): Add upgrade command to brew bundle script

--- home/.chezmoiscripts/run_after_101-brew-install.sh
+++ home/.chezmoiscripts/run_after_101-brew-install.sh
@@ -3,4 +3,5 @@
 if (( $+commands[brew] )); then
   echo "Installing libraries and applications with homebrew"
   brew bundle check --global || brew bundle install --global || true
+  brew upgrade || true
 fi
