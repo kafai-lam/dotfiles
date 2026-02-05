99a2a1adf3e34f1031ad3a071f57d83610431f6d
Fai <lamkafai1997@gmail.com>
2026-02-04 23:44:57 -0500

    chore(config): Update default working directories
    Update default working directories from ~/Developer to ~/Repositories
    in iTerm2 profiles and try plugin configuration to reflect new
    repository location. This change affects both window and tab
    directories in iTerm2, as well as the try plugin's path setting.
    
    This update aligns with the new repository organization structure.

--- home/dot_config/zsh/plugins/try.plugin.zsh
+++ home/dot_config/zsh/plugins/try.plugin.zsh
@@ -1,7 +1,7 @@
 if (( ! $+commands[try-rs] )); then return; fi
 
 export TRY_CONFIG_DIR="$XDG_CONFIG_HOME/try"
-export TRY_PATH="$HOME/Developer/tries"
+export TRY_PATH="$HOME/Repositories/tries"
 
 z4h source $TRY_CONFIG_DIR/try-rs.zsh
 
